5dc61ca02d1bd94239b34c6be483a8dcc89960dc
ApurvChatur <chaturapurv@gmail.com>
2024-03-20 05:23:43 +0530

    Lead

--- site/frontend/src/love/cComponent/aGlobalComponent/component/cLoader/index.jsx
+++ site/frontend/src/love/cComponent/aGlobalComponent/component/cLoader/index.jsx
@@ -4,7 +4,8 @@ import brand from '@/love/dFunction/gBrand';
 
 const Loader = ({ text }) => {
   return (
-    <div className="flex justify-center items-center h-96 text-gray-400 bg-gray-900 body-font">          
+    <div className="flex flex-col justify-center items-center h-screen body-font bg-[#FBF8BE] dark:bg-[#234E70] text-[#234E70] dark:text-[#FBF8BE]">          
+      <img src={brand().logo} alt={brand().name} className='w-1/12' />
       {text || "Loading..."}
     </div>
   )
